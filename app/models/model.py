0ac06fd333b6cb26c3a4f5c8bbe3f20b6b4118e5
EC2 Default User <ec2-user@ip-172-31-20-243.us-east-2.compute.internal>
2019-07-18 17:14:06 +0000

    We did it bois

--- app/models/model.py
+++ app/models/model.py
@@ -1,1 +1,17 @@
-# def drywall_calc()
+def calculate_dry_results(a1, a2, a3, a4, a5, a6):
+    total_int_values_in_list = int(a1)+int(a2)+int(a3)+int(a4)+int(a5)+int(a6)
+    if total_int_values_in_list<5:
+        return "dummy thiqué dry wall"
+    elif total_int_values_in_list<10:
+        return "Eco Friendly Drywall"
+    elif total_int_values_in_list<12:
+        return "Moisture Resistant Drywall"
+    elif total_int_values_in_list<14:
+        return "Sound Proof Drywall"
+    elif total_int_values_in_list<16:
+        return "Mold Resistant Drywall"
+    elif total_int_values_in_list<18:
+        return "Paperless Drywall"
+    elif total_int_values_in_list<100:
+        return "God Drywall"
+    return "dope dry wall"
--- app/routes.py
+++ app/routes.py
@@ -11,4 +11,14 @@ def index():
 def dry_wall():
     return render_template('drywall.html')
     
-# @app.route('/results_drywall')
+@app.route('/dry_results', methods=['GET', 'POST'])
+def results_drywall():
+    userdata = formopener.dict_from(request.form)
+    answer1 = userdata['answer1']
+    answer2 = userdata['answer2']
+    answer3 = userdata['answer3']
+    answer4 = userdata['answer4']
+    answer5 = userdata['answer5']
+    answer6 = userdata['answer6']
+    dry_total = model.calculate_dry_results(answer1, answer2, answer3, answer4, answer5, answer6)
+    return render_template('dry_results.html', dry_total = dry_total)
